6c4b754385a5460cbcdf31b28fea8210ccd027b9
DaanV2 <daanverstraten@hotmail.com>
2022-09-10 00:19:11 +0200

    Added String()

--- cache/items.go
+++ cache/items.go
@@ -3,15 +3,19 @@ package cache
 // CacheItemString an item that stores a string
 type CacheItemString string
 
+// NewCacheItemString creates a new CacheItemString.
+func NewCacheItemString(key string) *CacheItemString {
+	item := CacheItemString(key)
+	return &item
+}
+
 // GetKey returns the key of the KeyValuePair.
 func (c *CacheItemString) GetKey() string {
 	return string((*c))
 }
 
-// NewCacheItemString creates a new CacheItemString.
-func NewCacheItemString(key string) *CacheItemString {
-	item := CacheItemString(key)
-	return &item
+func (c *CacheItemString) String() string {
+	return string(*c)
 }
 
 // KeyValuePair is a key/value pair of a key and generic value.
